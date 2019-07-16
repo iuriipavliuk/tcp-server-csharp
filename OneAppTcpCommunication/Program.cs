6565bf6b8a35ccb16ff71fa96e64d8e052904724
Iurii Pavliuk <iurii.pavliuk@vw-fc.com>
2019-07-16 15:02:02 +0200

    - register pod server

--- OneAppTcpCommunication/Program.cs
+++ OneAppTcpCommunication/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace TcpServer
+﻿using System;
+
+namespace OneAppTcpCommunication
 {
   internal static class Program
   {
     public static void Main(string[] args)
     {
+      OneAppTcpServer server = new OneAppTcpServer(8080);
+      
+      server.AddRoute("register-pod", TcpRequestHandler);
+      
+      server.Start();
+    }
+
+    private static TcpResponse TcpRequestHandler(TcpRequest request)
+    {
+      Console.WriteLine($"Request: {request.url}, {request.payload}");
+      return new TcpResponse()
+      {
+        code = 0,
+        payload = "Pod is registered"
+      };
     }
   }
 }
