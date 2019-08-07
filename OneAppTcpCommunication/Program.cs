59f9bbe60a118d28c73646cc0c956b5f371c7dfc
Iurii Pavliuk <iurii.pavliuk@vw-fc.com>
2019-08-07 15:39:37 +0200

    - client program

--- OneAppTcpCommunication/Program.cs
+++ OneAppTcpCommunication/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace OneAppTcpCommunication
 {
-  internal static class Program
-  {
-    public static void Main(string[] args)
+    internal static class Program
     {
-            //JsonToCSharpProgram.Start();
+        public static void Main(string[] args)
+        {
             TcpServerProgram.Start();
+            Task.Delay(1000).Wait();
+            TcpClientProgram.Start();
+        }
     }
-  }
 }
